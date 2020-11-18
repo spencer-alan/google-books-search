d7cca59e02802242987d6e35d8ba123ea88e845a
b1acksmyth <smith.spence13@gmail.com>
2020-11-18 15:19:37 -0800

    set up for the application w/ some backend

---
+++ backend/models/book.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: { 
+    type: String, 
+    required: true 
+  },
+  author: { 
+    type: String, 
+    required: true 
+  },
+  description: String,
+  thumbnail: { 
+    type: String, 
+    default: "" 
+  },
+  href: {
+    type: String,
+    default: ""
+  }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
