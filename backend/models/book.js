8331760e4fc0f1ffd4dc518fda9159fba698343c
b1acksmyth <smith.spence13@gmail.com>
2020-11-25 12:46:48 -0800

    updated model to account for multiple authors

--- backend/models/book.js
+++ backend/models/book.js
@@ -2,23 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  title: { 
-    type: String, 
-    required: true 
-  },
-  author: { 
-    type: String, 
-    required: true 
-  },
-  description: String,
-  thumbnail: { 
-    type: String, 
-    default: "" 
-  },
-  link: {
-    type: String,
-    default: ""
-  }
+	title: {
+		type: String,
+		required: true,
+	},
+	author: {
+		type: [],
+		defualt: [""],
+	},
+	description: String,
+	thumbnail: {
+		type: String,
+		default: "",
+	},
+	link: {
+		type: String,
+		default: "",
+	},
 });
 
 const Book = mongoose.model("Book", bookSchema);
