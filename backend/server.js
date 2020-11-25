bd4b3919226074a037e2eff962f6386343a4068e
b1acksmyth <smith.spence13@gmail.com>
2020-11-25 12:47:42 -0800

    added socket.io to backend

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,43 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const PORT = process.env.PORT || 5000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
 }
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooklist");
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://localhost/googlebooklist",
+	{
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	}
+);
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+});
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+http.listen(PORT, function () {
+	console.log(`Server now listening on PORT ${PORT}!`);
 });
