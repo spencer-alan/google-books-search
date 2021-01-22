1f093ece0a09a23b4ec1cc7981de91ad05621a55
b1acksmyth <smith.spence13@gmail.com>
2021-01-22 13:03:40 -0800

    added styles to search bar

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,18 +1,33 @@
 import React from "react";
-import { Grid, Paper, Button, Typography } from "@material-ui/core";
+import { Grid, Paper, Button, Typography, TextField } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
 	center: {
 		textAlign: "center",
 	},
-	bigPaper: {
+	paper: {
 		minWidth: "90%",
 		padding: 25,
+	},
+	paperFlex: {
+		display: "flex",
+		minWidth: "90%",
+		padding: 25,
+		alignItems: "flex-end",
+		justifyContent: "space-between",
 	},
 	wrapper: {
 		marginTop: "3rem",
 		justifyContent: "center",
+	},
+	searchBar: {
+		width: "25rem",
+	},
+	button: {
+		margin: "12px",
+		backgroundColor: "#0080ff",
 	},
 }));
 
@@ -22,7 +37,7 @@ function Search() {
 	return (
 		<>
 			<Grid container spacing={2} className={classes.wrapper}>
-				<Paper className={classes.bigPaper}>
+				<Paper className={classes.paper}>
 					<Grid container spacing={2} className={classes.center}>
 						<Grid item xs={12}>
 							<Typography variant="h2">(React) Google Book Search</Typography>
@@ -35,9 +50,20 @@ function Search() {
 					</Grid>
 				</Paper>
 			</Grid>
-			<Grid container className={classes.wrapper}>
-				<Paper variant="outlined" className={classes.bigPaper}>
-					<Typography>Book Search</Typography>
+			<Grid
+				container
+				direction="row"
+				alignItems="flex-end"
+				className={classes.wrapper}
+			>
+				<Paper variant="outlined" className={classes.paperFlex}>
+					<Typography variant="h2">Book Search</Typography>
+					<div classname={classes.searchBar}>
+						<TextField label="Search" variant="standard" />
+						<Button className={classes.button}>
+							<SearchIcon />
+						</Button>
+					</div>
 				</Paper>
 			</Grid>
 		</>
