3f9373b1d6e1d4d4dd062e907e6e36a49411159c
b1acksmyth <smith.spence13@gmail.com>
2021-02-04 01:46:37 -0800

    linked buttons to different pages

--- client/src/components/Navbar/nav.js
+++ client/src/components/Navbar/nav.js
@@ -1,17 +1,34 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { AppBar, Button, Toolbar, Typography } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	title: {
+		flexGrow: 1,
+	},
+}));
 
 function Nav() {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography>
-          Google Books
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  )
+	const classes = useStyles();
+
+	return (
+		<AppBar position="static">
+			<Toolbar>
+				<Typography variant="h6" className={classes.title}>
+					Google Books
+				</Typography>
+				<Button color="inherit" href="/">
+					Home
+				</Button>
+				<Button color="inherit" href="/saved">
+					Saved Books
+				</Button>
+			</Toolbar>
+		</AppBar>
+	);
 }
 
 export default Nav;
