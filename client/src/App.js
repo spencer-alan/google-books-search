4e82f56a9ed837005998675493402640c4c47349
b1acksmyth <smith.spence13@gmail.com>
2021-02-05 00:57:02 -0800

    server fix tries

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Navbar/nav";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
 
-
 function App() {
-  return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/search"]}>
-            <Search />
-          </Route>
-          <Route exact path="/saved">
-            <Saved />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div>
+				<Nav />
+				<Switch>
+					<Route exact path={["/", "/search"]}>
+						<Search />
+					</Route>
+					<Route exact path="/saved">
+						<Saved />
+					</Route>
+					<Route>
+						<NoMatch />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
