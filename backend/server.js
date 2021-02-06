152f63896a81905d29f4eb58412f54a670ae43b2
b1acksmyth <smith.spence13@gmail.com>
2021-02-06 09:51:03 -0800

    server snahges for heroku

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,10 @@ app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/client/build")));
+	app.use(express.static(__dirname, "client/build"));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "/client/build/index.html"));
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 	});
 }
 
